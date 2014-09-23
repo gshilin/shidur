8bdda707182a505e1168245457c5b0f4b7519dfa
Gregory Shilin <gshilin@gmail.com>
2014-09-23 17:16:10 +0300

    bugfix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,6 +59,7 @@ function gotoBookmark(author, title, pageNo, slideNo) {
 
     if (target.length > 0) {
         var newpos = target.offset().top - $('.slides ul li').first().offset().top;
+        console.log("Animating to " + newpos);
         $('body').animate({
             scrollTop: newpos
         }, 500);
