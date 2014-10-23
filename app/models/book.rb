16c9d7d5ce7eadc35e62d1d966d8d7dc6d34eb7c
Gregory Shilin <gshilin@gmail.com>
2014-10-23 11:28:12 +0300

    do not permit number of lines on one slide to be > 4

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,48 +11,24 @@
 #  slides     :text
 #
 
+class ContentValidator < ActiveModel::Validator
+  def validate(record)
+    success, errors = Book.parse_data_for_slides(record.content)
+    errors.each do |page, letter, subletter|
+      record.errors[:base] << "Too many lines on page #{page} letter #{letter}-#{subletter}"
+    end unless success
+  end
+end
+
 class Book < ActiveRecord::Base
+
+  validates_with ContentValidator
 
   def content=(data)
     super
 
-    slides        = []
-    page          = 0
-    letter        = 0
-    subletter     = 0
-    slide_content = []
-    data.split(/\n|\r\n/).each do |line|
-      case
-        when line =~ /^\s*$/
-        when line =~ /^%author/
-        when line =~ /^%book/
-        when line =~ /^%page\s+(.+)\s*$/
-          page = $1
-          unless slide_content.blank?
-            subletter += 1
-            slides << add_content(page, letter, subletter, slide_content)
-            slide_content = []
-          end
-        when line =~ /^%letter\s+(.+)\s*$/
-          unless slide_content.blank?
-            subletter += 1
-            slides << add_content(page, letter, subletter, slide_content)
-            slide_content = []
-          end
-          letter    = $1
-          subletter = 0
-        when line =~ /^%break$/
-          unless slide_content.blank?
-            subletter += 1
-            slides << add_content(page, letter, subletter, slide_content)
-            slide_content = []
-          end
-        else
-          slide_content << line.gsub(/'/, '&#39;').gsub(/^(\.\d+(\/\d+)*)\s/, "<bdi dir=\"ltr\">\\1</bdi>&nbsp;")
-      end
-    end
-    slides << add_content(page, letter, subletter, slide_content) unless slide_content.blank?
-    self.slides = slides.join('')
+    result, slides = Book.parse_data_for_slides(data)
+    write_attribute :slides, slides.join('') if result
   end
 
   def self.authors
@@ -68,8 +44,55 @@ class Book < ActiveRecord::Base
   end
 
   private
-  def add_content(page, letter, subletter, content)
+
+  def self.add_content(page, letter, subletter, content)
     "<li class=\"draggable\" data-page=\"#{page}\" data-letter=\"#{letter}#{subletter == 1 ? '' : "-#{subletter}"}\"><div class=\"wrap\"><div class=\"backdrop\"><span class=\"handle glyphicon glyphicon-move\"></span>" + content.join('<br/>') + '</div></div></li>'
   end
 
+  def self.parse_data_for_slides(data)
+    result        = []
+    slides        = []
+    page          = 0
+    letter        = 0
+    subletter     = 0
+    slide_content = []
+    slide_lines   = 0
+    data.split(/\n|\r\n/).each do |line|
+      case
+        when line =~ /^\s*$/
+        when line =~ /^%author/
+        when line =~ /^%book/
+        when line =~ /^%page\s+(.+)\s*$/
+          page = $1
+          unless slide_content.blank?
+            subletter += 1
+            slides << add_content(page, letter, subletter, slide_content)
+            slide_content = []
+            slide_lines   = 0
+          end
+        when line =~ /^%letter\s+(.+)\s*$/
+          unless slide_content.blank?
+            subletter += 1
+            slides << add_content(page, letter, subletter, slide_content)
+            slide_content = []
+            slide_lines   = 0
+          end
+          letter    = $1
+          subletter = 0
+        when line =~ /^%break$/
+          unless slide_content.blank?
+            subletter += 1
+            slides << add_content(page, letter, subletter, slide_content)
+            slide_content = []
+            slide_lines   = 0
+          end
+        else
+          slide_content << line.gsub(/'/, '&#39;').gsub(/^(\.\d+(\/\d+)*)\s/, "<bdi dir=\"ltr\">\\1</bdi>&nbsp;")
+          slide_lines += 1
+          result << [page, letter, subletter] if slide_lines > 4
+      end
+    end
+    slides << add_content(page, letter, subletter, slide_content) unless slide_content.blank?
+    result.blank? ? [true, slides] : [false, result]
+  end
 end
